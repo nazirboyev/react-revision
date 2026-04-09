c7ad399e89e7221317d366c9aceedd4c08d09f59
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-04-09 17:33:11 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router";
-import Header from "./components/Header";
+import Header from './components/header.jsx'
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Stopwatch from "./pages/Stopwatch";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
-        <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
+          <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
+        </Route>
       </Routes>
     </>
   );
