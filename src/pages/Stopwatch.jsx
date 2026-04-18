da414af3fe935bd60709e260e05ac9c60c284219
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-04-18 16:46:56 +0500

    done

--- src/pages/Stopwatch.jsx
+++ src/pages/Stopwatch.jsx
@@ -3,20 +3,27 @@ import { useState } from "react";
 function Stopwatch() {
   const [timer, setTimer] = useState(false);
   const [time, setTime] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
   const runTimer = () => {
-    console.log("clicked...");
+    if (!timer) {
+      let intervalId = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1);
+
+      setIntervalId(intervalId);
+    } else {
+      clearInterval(intervalId);
+    }
+
     setTimer(!timer);
-    setInterval(() => {
-      setTime((prev) => prev + 1);
-    }, 100);
   };
 
   const reset = () => {
     console.log("clicked...");
     setTimer(false);
-    setTime(0)
-    clearInterval()
+    setTime(0);
+    clearInterval(intervalId);
   };
 
   return (
@@ -24,8 +31,17 @@ function Stopwatch() {
       <div className="container">
         <h1> Stopwatch</h1>
 
-        <h1 className="timer">00:00:00</h1>
-        <h1 className="timer">{time}</h1>
+        <h1 className="timer">
+          {Math.floor(time / 3600) < 10
+            ? "0" + Math.floor(time / 3600)
+            : Math.floor(time / 3600)}
+          :
+          {Math.floor(time / 60) % 60 < 10
+            ? "0" + (Math.floor(time / 60) % 60)
+            : Math.floor(time / 60) % 60}
+          :{time % 60 < 10 ? "0" + (time % 60) : time % 60}
+        </h1>
+        {/* <h1 className="timer">{time}</h1> */}
         <button
           onClick={runTimer}
           className={`btn ${timer ? "btn-stop" : "btn-start"}`}
