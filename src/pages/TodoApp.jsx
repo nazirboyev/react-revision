bcfa1f9d4be181b1438fdd595a477c3096b38752
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-04-18 17:56:19 +0500

    done

--- src/pages/TodoApp.jsx
+++ src/pages/TodoApp.jsx
@@ -4,6 +4,39 @@ function TodoApp() {
   return (
     <>
       <h1>2. TodoApp</h1>
+      <div className="head">
+          <input className="input" type="text" placeholder="Add a task" />
+          <button className="button">Add</button>
+      </div>
+      <div className="body">
+        <div className="boxes">
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+          <div className="box">
+            <h3>Text</h3>
+            <button className="box-btn">Delete</button>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
