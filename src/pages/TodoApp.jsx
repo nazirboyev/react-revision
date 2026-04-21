1a1faf935cf8f429da4b03767ea9a0c35fa4e4ea
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-04-21 18:20:32 +0500

    done

--- src/pages/TodoApp.jsx
+++ src/pages/TodoApp.jsx
@@ -1,42 +1,59 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 function TodoApp() {
+  const [todoText, setTodoText] = useState("");
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Go to school",
+    },
+  ]);
+
+  const onAddBtnClick = () => {
+    console.log(todoText);
+
+    let newTodo = {
+      title: todoText,
+      id: todos.length + 7,
+    };
+
+    setTodos([...todos, newTodo]);
+
+    setTodoText("");
+  };
+
+  const onDeleteBtnClick = (id) => {
+    console.log(`Delete ${id}`);
+
+    let filteredTodos = todos.filter((t) => t.id !== id);
+
+    setTodos(filteredTodos);
+  };
+
   return (
     <>
       <h1>2. TodoApp</h1>
-      <div className="head">
-          <input className="input" type="text" placeholder="Add a task" />
-          <button className="button">Add</button>
+      <div className="input">
+        <input
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+          type="text"
+          placeholder="Add a task"
+        />
+        <button className="bnt-input" onClick={onAddBtnClick}>Add</button>
       </div>
-      <div className="body">
-        <div className="boxes">
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-          <div className="box">
-            <h3>Text</h3>
-            <button className="box-btn">Delete</button>
-          </div>
-        </div>
+      <div className="Board">
+        {todos.map((t) => {
+          return (
+            <div key={t.id} className="BoardBox">
+              <h1> {t.title} </h1>
+              <button className="box-bnt" onClick={() => onDeleteBtnClick(t.id)}>Delete</button>
+            </div>
+          );
+        })}
       </div>
-
     </>
   );
 }
