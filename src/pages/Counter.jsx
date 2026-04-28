b0f49565a6a82da0d4d473d6c585f9a85d4d762a
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-04-28 17:31:17 +0500

    done

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function CounterApp() {
+      const [count, setCount] = useState(0);
+
+  const plus = () => setCount((prev) => prev + 1);
+
+  const minus = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+    
     return(
         <>
-            <div className="count-container">
+            <div className="count-container wrap-counter">
                 <div className="header-count">
                     <i className="icon" class="fa-solid fa-cart-shopping"></i>
                     <h1 className="items-number">0</h1>
@@ -12,28 +24,12 @@ function CounterApp() {
                     <button className="refresh2-btn "><i class="fa-solid fa-arrows-spin"></i></button>    
                 </div>
                 <div className="legs-count">
-                    <h1 className="Zero">Zero</h1>
-                    <button className="bnt-1"><i class="fa-solid fa-circle-plus"></i></button>
-                    <button className="bnt-2"><i class="fa-solid fa-circle-minus"></i></button>
+                    <h1 className="Zero"><span >{count}</span></h1>
+                    <span className="count">{count}</span>
+                    <button onClick={() => plus()} className="bnt-1"><i class="fa-solid fa-circle-plus"></i></button>
+                    <button onClick={() => minus()} className="bnt-2"><i class="fa-solid fa-circle-minus"></i></button>
                     <button className="bnt-3"><i class="fa-solid fa-trash-can"></i></button>
-                </div>
-                <div className="legs-count">
-                    <h1 className="Zero">Zero</h1>
-                    <button className="bnt-1"><i class="fa-solid fa-circle-plus"></i></button>
-                    <button className="bnt-2"><i class="fa-solid fa-circle-minus"></i></button>
-                    <button className="bnt-3"><i class="fa-solid fa-trash-can"></i></button>
-                </div>
-                <div className="legs-count">
-                    <h1 className="Zero">Zero</h1>
-                    <button className="bnt-1"><i class="fa-solid fa-circle-plus"></i></button>
-                    <button className="bnt-2"><i class="fa-solid fa-circle-minus"></i></button>
-                    <button className="bnt-3"><i class="fa-solid fa-trash-can"></i></button>
-                </div><div className="legs-count">
-                    <h1 className="Zero">Zero</h1>
-                    <button className="bnt-1"><i class="fa-solid fa-circle-plus"></i></button>
-                    <button className="bnt-2"><i class="fa-solid fa-circle-minus"></i></button>
-                    <button className="bnt-3"><i class="fa-solid fa-trash-can"></i></button>
-                </div>            
+                </div>         
             </div>
 
         </>
